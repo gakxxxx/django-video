17506d5dc2f1d8f6730de0171663e8b65c83275e
root <root@video01.poclab.local>
2020-05-23 19:07:55 +0900

    first commit

---
+++ video/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+
+def index(request):
+    return render(request, 'video/index.html')
+
+from django.db.models import Count
+
+def construct_page(request, all_content_ids, page_contents, current_page, max_page, url_type, url_word=''):
+    # page_contents(動画)に関連するタグを抜き出し、テンプレートで使えるよう整形
+    contents = []
+    for item in page_contents:
+        tmp_dict = item
+        tmp_dict.update({'tags': VideoTagList.objects.filter(content_id=item['id']).select_related('tag')})
+        contents.append(tmp_dict)
+
+    # all_content_idsからタグを多い順で集計し、整形する
+    tag_cnt = VideoTagList.objects.filter(content__in = all_content_ids).values('tag').annotate(tag_count=Count('tag')).order_by('-tag_count')[:10]
+    tag_names = [VideoTagName.objects.filter(id = item.get('tag'))[0] for item in tag_cnt]
+    tags = [{'name': tag_names[i].name, 'count': tag_cnt[i]["tag_count"]} for i in range(len(tag_names))]
+
+    # ページが有効な範囲をvalidでマークを付ける
+    page_list = [{'num':x, 'valid':0 <= x and x <= max_page} for x in range(current_page-5, current_page+4)]
+
+    return render(request, 'video/index.html', {'tags': tags, 'contents': contents, 'page':{'type':url_type, 'word': url_word, 'current': current_page, 'max': max_page, 'list': page_list}})
